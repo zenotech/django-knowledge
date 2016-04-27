a254ec51e313180ce97865f833171cc221fa219e
James Sharpe <james.sharpe@zenotech.com>
2016-04-27 16:10:17 +0100

    Update to use django migrations

--- knowledge/migrations/0001_initial.py
+++ knowledge/migrations/0001_initial.py
@@ -1,150 +1,73 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
-from knowledge.utils import user_model_label
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        
-        # Adding model 'Category'
-        db.create_table('knowledge_category', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('lastchanged', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
-        ))
-        db.send_create_signal('knowledge', ['Category'])
-
-        # Adding model 'Question'
-        db.create_table('knowledge_question', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('lastchanged', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm[user_model_label], null=True, blank=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=64, null=True, blank=True)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, null=True, blank=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('body', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('status', self.gf('django.db.models.fields.CharField')(default='private', max_length=32, db_index=True)),
-            ('locked', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('knowledge', ['Question'])
-
-        # Adding M2M table for field categories on 'Question'
-        db.create_table('knowledge_question_categories', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('question', models.ForeignKey(orm['knowledge.question'], null=False)),
-            ('category', models.ForeignKey(orm['knowledge.category'], null=False))
-        ))
-        db.create_unique('knowledge_question_categories', ['question_id', 'category_id'])
-
-        # Adding model 'Response'
-        db.create_table('knowledge_response', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('lastchanged', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm[user_model_label], null=True, blank=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=64, null=True, blank=True)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, null=True, blank=True)),
-            ('question', self.gf('django.db.models.fields.related.ForeignKey')(related_name='responses', to=orm['knowledge.Question'])),
-            ('body', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('status', self.gf('django.db.models.fields.CharField')(default='inherit', max_length=32, db_index=True)),
-            ('accepted', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('knowledge', ['Response'])
+from django.db import migrations, models
+from django.conf import settings
 
 
-    def backwards(self, orm):
-        
-        # Deleting model 'Category'
-        db.delete_table('knowledge_category')
+class Migration(migrations.Migration):
 
-        # Deleting model 'Question'
-        db.delete_table('knowledge_question')
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-        # Removing M2M table for field categories on 'Question'
-        db.delete_table('knowledge_question_categories')
-
-        # Deleting model 'Response'
-        db.delete_table('knowledge_response')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        user_model_label: {
-            'Meta': {'object_name': user_model_label.split('.')[-1]},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'knowledge.category': {
-            'Meta': {'object_name': 'Category'},
-            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'lastchanged': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        'knowledge.question': {
-            'Meta': {'ordering': "['-added']", 'object_name': 'Question'},
-            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'body': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['knowledge.Category']", 'symmetrical': 'False'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'lastchanged': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'locked': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
-            'status': ('django.db.models.fields.CharField', [], {'default': "'private'", 'max_length': '32', 'db_index': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm[user_model_label]", 'null': 'True', 'blank': 'True'})
-        },
-        'knowledge.response': {
-            'Meta': {'ordering': "['added']", 'object_name': 'Response'},
-            'accepted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'body': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'lastchanged': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
-            'question': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'responses'", 'to': "orm['knowledge.Question']"}),
-            'status': ('django.db.models.fields.CharField', [], {'default': "'inherit'", 'max_length': '32', 'db_index': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm[user_model_label]", 'null': 'True', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['knowledge']
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('added', models.DateTimeField(auto_now_add=True)),
+                ('lastchanged', models.DateTimeField(auto_now=True)),
+                ('title', models.CharField(max_length=255)),
+                ('slug', models.SlugField(unique=True)),
+            ],
+            options={
+                'ordering': ['title'],
+                'verbose_name': 'Category',
+                'verbose_name_plural': 'Categories',
+            },
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('added', models.DateTimeField(auto_now_add=True)),
+                ('lastchanged', models.DateTimeField(auto_now=True)),
+                ('alert', models.BooleanField(default=True, help_text='Check this if you want to be alerted when a new response is added.', verbose_name='Alert')),
+                ('name', models.CharField(help_text='Enter your first and last name.', max_length=64, null=True, verbose_name='Name', blank=True)),
+                ('email', models.EmailField(help_text='Enter a valid email address.', max_length=254, null=True, verbose_name='Email', blank=True)),
+                ('title', models.CharField(help_text='Enter your question or suggestion.', max_length=255, verbose_name='Question')),
+                ('body', models.TextField(help_text='Please offer details. Markdown enabled.', null=True, verbose_name='Description', blank=True)),
+                ('status', models.CharField(default=b'private', max_length=32, verbose_name='Status', db_index=True, choices=[(b'public', 'Public'), (b'private', 'Private'), (b'internal', 'Internal')])),
+                ('locked', models.BooleanField(default=False)),
+                ('categories', models.ManyToManyField(to='knowledge.Category', blank=True)),
+                ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'ordering': ['-added'],
+                'verbose_name': 'Question',
+                'verbose_name_plural': 'Questions',
+            },
+        ),
+        migrations.CreateModel(
+            name='Response',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('added', models.DateTimeField(auto_now_add=True)),
+                ('lastchanged', models.DateTimeField(auto_now=True)),
+                ('alert', models.BooleanField(default=True, help_text='Check this if you want to be alerted when a new response is added.', verbose_name='Alert')),
+                ('name', models.CharField(help_text='Enter your first and last name.', max_length=64, null=True, verbose_name='Name', blank=True)),
+                ('email', models.EmailField(help_text='Enter a valid email address.', max_length=254, null=True, verbose_name='Email', blank=True)),
+                ('body', models.TextField(help_text='Please enter your response. Markdown enabled.', null=True, verbose_name='Response', blank=True)),
+                ('status', models.CharField(default=b'inherit', max_length=32, verbose_name='Status', db_index=True, choices=[(b'public', 'Public'), (b'private', 'Private'), (b'internal', 'Internal'), (b'inherit', 'Inherit')])),
+                ('accepted', models.BooleanField(default=False)),
+                ('question', models.ForeignKey(related_name='responses', to='knowledge.Question')),
+                ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'ordering': ['added'],
+                'verbose_name': 'Response',
+                'verbose_name_plural': 'Responses',
+            },
+        ),
+    ]
